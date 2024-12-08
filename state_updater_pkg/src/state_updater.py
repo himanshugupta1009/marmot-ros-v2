a7e0dcaa5c6d1080874b86ffa8e26d79edfe7dd3
william-pope <william.pope@colorado.edu>
2024-12-07 23:43:55 -0700

    Add Ansh's modified state_updater.py code

--- state_updater_pkg/src/state_updater.py
+++ state_updater_pkg/src/state_updater.py
@@ -10,6 +10,9 @@ from scipy.spatial.transform import Rotation
 from numpy import linalg as la
 from datetime import datetime
 import csv
+import logging
+import os
+import time
 
 # File Overview:
 # - subscribes to Pose topic for each object tracked by Vicon
@@ -28,142 +31,180 @@ import csv
 #   ...]
 # - length = 4 + 2*n_peds
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class StateUpdater:
-    current_veh_pose_msg = []
-    current_veh_twist_msg = []
-    current_ped1_msg = []
-    current_ped2_msg = []
-    current_ped3_msg = []
-    current_ped4_msg = []
 
-    record_hist = False
-    saved_hist = False
 
-    hist_veh_pose_msg = []
-    hist_veh_twist_msg = []
-    hist_ped1_msg = []
-    hist_ped2_msg = []
-    hist_ped3_msg = []
-    hist_ped4_msg = []
+    # A class to manage the state of a vehicle and multiple pedestrians based on Vicon data.
 
-    def __init__(self):
-        # subscribe to Vicon topic for each object
-        # - vehicle
-        self.vrpn_sub_marmot_pose = rospy.Subscriber(
-            "/car/vrpn_client_ros/vrpn_client_node/ADCL_Marmot/pose", 
-            PoseStamped,
-            callback=self.store_current_msg,
-            callback_args=0,
-            queue_size=1)
+    # This class subscribes to Pose and Twist topics for a vehicle and multiple pedestrians,
+    # stores their current states, and provides a service to update and retrieve the state vector.
+    # The state vector includes the vehicle's position, orientation, and velocity, as well as
+    # the positions of the pedestrians.
 
-        self.vrpn_sub_marmot_twist = rospy.Subscriber(
-            "/car/vrpn_client_ros/vrpn_client_node/ADCL_Marmot/twist", 
-            TwistStamped,
-            callback=self.store_current_msg,
-            callback_args=1,
-            queue_size=1)
+    # Attributes:
+    #     num_pedestrians (int): The number of pedestrians to track.
+    #     current_veh_msg (PoseStamped): The latest Pose message for the vehicle.
+    #     current_veh_twist_msg (TwistStamped): The latest Twist message for the vehicle.
+    #     current_ped_msgs (list): List of the latest Pose messages for each pedestrian.
+    #     hist_veh_msg (list): History of vehicle Pose messages.
+    #     hist_ped_msgs (list): List of histories for each pedestrian's Pose messages.
+    #     record_hist (bool): Flag to indicate if history recording is active.
+    #     saved_hist (bool): Flag to indicate if history has been saved.
 
-        # - pedestrians
-        self.vrpn_sub_ped1_pose = rospy.Subscriber(
-            "/car/vrpn_client_ros/vrpn_client_node/ADCL_Ped1/pose", 
-            PoseStamped,
-            callback=self.store_current_msg,
-            callback_args=2,
-            queue_size=1)
 
-        self.vrpn_sub_ped2_pose = rospy.Subscriber(
-            "/car/vrpn_client_ros/vrpn_client_node/ADCL_Ped2/pose", 
-            PoseStamped,
-            callback=self.store_current_msg,
-            callback_args=3,
-            queue_size=1)
+    def __init__(self, num_pedestrians):
+        try:
+            #subscribe to Vicon topic for each object
 
-        self.vrpn_sub_ped3_pose = rospy.Subscriber(
-            "/car/vrpn_client_ros/vrpn_client_node/ADCL_Ped3/pose", 
-            PoseStamped,
-            callback=self.store_current_msg,
-            callback_args=4,
-            queue_size=1)
+            # Store the number of pedestrians
+            self.num_pedestrians = num_pedestrians
+            
+            # Initialize vehicle data
+            self.current_veh_msg = None
+            self.current_veh_twist_msg = None
+            self.hist_veh_msg = []
+            self.hist_veh_twist_msg = []
+            
+            # Initialize pedestrian data containers
+            self.ped_subs = []            # List of pedestrian subscribers
+            self.current_ped_msgs = [None] * num_pedestrians  # Latest message for each pedestrian
+            self.hist_ped_msgs = [[] for _ in range(num_pedestrians)]  # History for each pedestrian
+            
+            self.record_hist = False
+            self.saved_hist = False
 
-        self.vrpn_sub_ped4_pose = rospy.Subscriber(
-            "/car/vrpn_client_ros/vrpn_client_node/ADCL_Ped4/pose", 
-            PoseStamped,
-            callback=self.store_current_msg,
-            callback_args=5,
-            queue_size=1)
+            # Vehicle subscriber
+            self.vrpn_sub_marmot_pose = rospy.Subscriber(
+                "/car/vrpn_client_ros/vrpn_client_node/ADCL_Marmot/pose", 
+                PoseStamped,
+                callback=self.store_current_msg,
+                callback_args='vehicle_pose',
+                queue_size=1)
 
-        # establish state_updater service as provider
-        self.update_state_srv = rospy.Service(
-            "/car/state_updater/get_state_update",
-            UpdateState,
-            self.update_state)
+            # Vehicle twist subscriber
+            self.vrpn_sub_marmot_twist = rospy.Subscriber(
+                "/car/vrpn_client_ros/vrpn_client_node/ADCL_Marmot/twist", 
+                TwistStamped,
+                callback=self.store_current_msg,
+                callback_args='vehicle_twist',
+                queue_size=1)
+            
+            # Pedestrian subscribers
+            for i in range(self.num_pedestrians):
+                topic_name = "/car/vrpn_client_ros/vrpn_client_node/ADCL_Ped{}/pose".format(i+1)
+                sub = rospy.Subscriber(
+                    topic_name, 
+                    PoseStamped,
+                    callback=self.store_current_msg,
+                    callback_args=i,
+                    queue_size=1)
+                self.ped_subs.append(sub)
 
-        return
-
-    # subscriber callback function
-    def store_current_msg(self, msg, vrpn_object):
-        # - vehicle
-        if vrpn_object == 0:
-            self.current_veh_pose_msg = copy.deepcopy(msg)
-            if self.record_hist == True:
-                self.hist_veh_pose_msg.append(msg)
-
-        elif vrpn_object == 1:
-            self.current_veh_twist_msg = copy.deepcopy(msg)
-            if self.record_hist == True:
-                self.hist_veh_twist_msg.append(msg)
-
-        # - pedestrians
-        elif vrpn_object == 2:
-            self.current_ped1_msg = copy.deepcopy(msg)
-            if self.record_hist == True:
-                self.hist_ped1_msg.append(msg)
-
-        elif vrpn_object == 3:
-            self.current_ped2_msg = copy.deepcopy(msg)
-            if self.record_hist == True:
-                self.hist_ped2_msg.append(msg)
-
-        elif vrpn_object == 4:
-            self.current_ped3_msg = copy.deepcopy(msg)
-            if self.record_hist == True:
-                self.hist_ped3_msg.append(msg)
-                
-        elif vrpn_object == 5:
-            self.current_ped4_msg = copy.deepcopy(msg)
-            if self.record_hist == True:
-                self.hist_ped4_msg.append(msg)
+            # Service for state estimation
+            self.update_state_srv = rospy.Service(
+                "/car/state_updater/get_state_update",
+                UpdateState,
+                self.update_state)
+            
+        except Exception as e:
+            rospy.logerr("Failed to initialize StateUpdater: %s", str(e))    
         
-        return
+
+
+
+        # subscriber callback function
+        # Callback function to store the current Pose message for the vehicle or pedestrians.
+
+        # Args:
+        #     pose_msg (PoseStamped): The Pose message received from the subscriber.
+        #     obj_id (str or int): Identifier for the object type (vehicle or pedestrian index).
+     
+    def store_current_msg(self, pose_msg, obj_id):
+        logging.debug("store_current_msg called with obj_id=%s", obj_id)
+
+        if isinstance(obj_id, str):  # Handle vehicle messages
+            if obj_id == 'vehicle_pose':
+                self.current_veh_msg = copy.deepcopy(pose_msg)
+                if self.record_hist:
+                    self.hist_veh_msg.append(pose_msg)
+            elif obj_id == 'vehicle_twist':
+                self.current_veh_twist_msg = copy.deepcopy(pose_msg)
+                if self.record_hist:
+                    self.hist_veh_twist_msg.append(pose_msg)
+            else:
+                rospy.logwarn("Unknown obj_id string: %s (type=%s)", obj_id, type(obj_id))
+
+        elif isinstance(obj_id, int):  # Handle pedestrian messages
+            if 0 <= obj_id < self.num_pedestrians:  # Ensure valid index
+                self.current_ped_msgs[obj_id] = copy.deepcopy(pose_msg)
+                if self.record_hist:
+                    self.hist_ped_msgs[obj_id].append(pose_msg)
+            else:
+                rospy.logwarn("Invalid pedestrian index: %s (type=%s)", obj_id, type(obj_id))
+        else:
+            rospy.logwarn("Unexpected obj_id type: %s (type=%s)", obj_id, type(obj_id))
+
+
+
+
 
     # function called by service
+
+    #     Service callback to update and return the current state vector.
+
+    #     Args:
+    #         req (UpdateState): The service request containing the record flag.
+
+    #     Returns:
+    #         UpdateStateResponse: The response containing the current state vector and pedestrian presence.
+    
     def update_state(self, req):
         self.record_hist = req.record
 
+      # data logging for times when recording has stopped and data needs saving
         if self.record_hist == False and self.saved_hist == False and len(self.hist_veh_msg) > 0: 
             self.save_s_hist()
 
-        current_state = [0]*(4 + 2*(0))
-        peds_in_env = [False]*4
+        # Calculate length of state vector based on number of pedestrians
+        current_state = [0] * (4 + 2 * self.num_pedestrians)
+        peds_in_env = [False] * self.num_pedestrians
+        
+        # Vehicle state
+        if self.current_veh_msg and self.current_veh_twist_msg:
+            current_state[0:4] = self.convert_veh_state(self.current_veh_msg, self.current_veh_twist_msg)
 
-        # - vehicle
-        current_state[0:4] = self.convert_veh_state(self.current_veh_pose_msg, self.current_veh_twist_msg)
+        # Pedestrian states
+        for i, ped_msg in enumerate(self.current_ped_msgs):
+            if ped_msg:
+                ped_state, ped_in_env = self.convert_ped_state(ped_msg)
+                current_state[4 + 2 * i:6 + 2 * i] = ped_state
+                peds_in_env[i] = ped_in_env  # Track whether the pedestrian is in the environment
+        
+        return UpdateStateResponse(state=current_state, peds_in_env=peds_in_env)
 
-        # - pedestrians
-        current_state[4:6], peds_in_env[0] = self.convert_ped_state(self.current_ped1_msg)
-        current_state[6:8], peds_in_env[1] = self.convert_ped_state(self.current_ped2_msg)
-        current_state[8:10], peds_in_env[2] = self.convert_ped_state(self.current_ped3_msg)
-        current_state[10:12], peds_in_env[3] = self.convert_ped_state(self.current_ped4_msg)
 
-        return UpdateStateResponse(current_state, peds_in_env)
+
 
     # converts ROS PoseStamped message to regular (x,y,theta,v) variables
+        # Converts ROS PoseStamped and TwistStamped messages to a vehicle state vector.
+
+        # Args:
+        #     pose_msg (PoseStamped): The Pose message for the vehicle.
+        #     twist_msg (TwistStamped): The Twist message for the vehicle.
+
+        # Returns:
+        #     state: A list containing the vehicle's x, y, theta, and velocity
+
     def convert_veh_state(self, pose_msg, twist_msg):
         ori = pose_msg.pose.orientation
         rot = Rotation.from_quat([ori.x, ori.y, ori.z, ori.w])
         rot_euler = rot.as_euler('xyz', degrees=False)
         theta = rot_euler[2]
 
+    
         y_cal = -0.162  # m from Vicon model center to rear axis
         x = pose_msg.pose.position.x + y_cal*math.cos(theta)
         y = pose_msg.pose.position.y + y_cal*math.sin(theta)
@@ -173,7 +214,16 @@ class StateUpdater:
         state = [x, y, theta, v]
 
         return state
+    
 
+
+# Converts a ROS PoseStamped message to a pedestrian state vector.
+
+#         Args:
+#             pose_msg (PoseStamped): The Pose message for the pedestrian.
+
+#         Returns:
+#             state: The pedestrian's state vector and a boolean indicating if the pedestrian is in the environment.
     def convert_ped_state(self, pose_msg):
         x = pose_msg.pose.position.x
         y = pose_msg.pose.position.y
@@ -182,69 +232,83 @@ class StateUpdater:
 
         # TO-DO: come up with conditon to check if Vicon reads ped within environment
         #   - may read spare helmet outside workspace in storage area, or may return 0,0 or something if not visible at all
-        if True:
-            ped_in_env = True
-        else:
-            ped_in_env = False
+        ped_in_env = (x != 0 or y != 0)
 
         return state, ped_in_env
+    
+
 
     # saves full Vicon history to csv file, called once at end of execution
     # TO-DO: manage histories from all objects
     #   - histories not necessarily same length, same time steps (hmm...)
     #   - see what each length is, might be close enough to not cause issues
     #   - probably easiest to save in separate files, pull together in analysis script
+
+
+
     def save_s_hist(self):
-        # veh_secs = datetime.fromtimestamp(self.current_veh_msg.header.stamp.secs)  
-        # veh_nsecs = datetime.fromtimestamp(self.current_veh_msg.header.stamp.nsecs)
-        # ped1_secs = datetime.fromtimestamp(self.current_ped1_msg.header.stamp.secs)  
-        # ped1_nsecs = datetime.fromtimestamp(self.current_ped1_msg.header.stamp.nsecs)
+        # Saves the history of vehicle and pedestrian states to CSV files.
+        # This function is called once at the end of execution to store the recorded history.
 
-        # print(veh_secs)
-        # print(veh_nsecs)
-        # print(ped1_secs)
-        # print(ped1_nsecs)
+            timestamp = datetime.now().isoformat()
+            base_dir = "/home/adcl/catkin_ws/src/marmot-ros-v2/controller_pkg/histories/"
+            dir_name = os.path.join(base_dir, timestamp)
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name) #Only creates the directory if it doesn't exist
 
-        # TO-DO: add datetime to file name
-        # TO-DO: add time stamp to each entry
 
-        hist_path = "/home/adcl/catkin_ws/src/marmot-ros-v2/controller_pkg/histories/"
+            #vehicles history
+            veh_filename = os.path.join(dir_name, "veh_hist_vrpn.csv")
+            ped_filename = [os.path.join(dir_name, "ped{}_hist_vrpn.csv".format(i+1)) for i in range(self.num_pedestrians)]
 
-        # veh history
-        f = open(hist_path + "veh_hist_vrpn.csv", 'w')
-        writer = csv.writer(f)
+            with open(veh_filename, 'w') as veh_file:
+                writer = csv.writer(veh_file)
+                # Write header row
+                writer.writerow(['timestamp', 'x', 'y', 'theta', 'v'])
 
-        veh_hist_len = min(len(self.hist_veh_pose_msg), len(self.hist_veh_twist_msg))
-        
-        for k in range(0, veh_hist_len):
-            pose_msg_k = self.hist_veh_pose_msg[k]
-            twist_msg_k = self.hist_veh_twist_msg[k]
+                veh_hist_len = min(len(self.hist_veh_msg), len(self.hist_veh_twist_msg))
+            
+                for k in range(veh_hist_len):
 
-            s_k = self.convert_veh_state(pose_msg_k, twist_msg_k)
+                        pose_msg_k = self.hist_veh_msg[k]
+                        twist_msg_k = self.hist_veh_twist_msg[k]
 
-            writer.writerow(s_k)
-        f.close()
+                        timestamp = pose_msg_k.header.stamp.to_sec()
+                        formatted_timestamp = datetime.fromtimestamp(timestamp).isoformat()
 
-        # ped1 history
-        f = open(hist_path + "ped1_hist_vrpn.csv", 'w')
-        writer = csv.writer(f)
-        for msg_k in self.hist_ped1_msg:
-            s_k = self.convert_ped_state(msg_k)
-            writer.writerow(s_k)
-        f.close()
-        
-        self.saved_hist = True
-        print("save complete")
+                        s_k = self.convert_veh_state(pose_msg_k, twist_msg_k)  # Pass both arguments
+                        writer.writerow([formatted_timestamp] + s_k)
 
-        return
+            # pedestrian histories
+            for i,ped_filename in enumerate(ped_filename):
+                with open(ped_filename, 'w') as ped_file:
+                    writer = csv.writer(ped_file)
+                    writer.writerow(['timestamp', 'x', 'y'])
+                    for msg in self.hist_ped_msgs[i]:
+                            timestamp = msg.header.stamp.to_sec()
+                            formatted_timestamp = datetime.fromtimestamp(timestamp).isoformat()
+                            writer.writerow([formatted_timestamp] + self.convert_ped_state(msg)[0])
+
+            self.saved_hist = True
+
+            print("Save complete for vehicle and pedestrian histories")
+            return
+
+
+
 
 if __name__ == '__main__':
     try:
         rospy.init_node("state_updater")
 
+        # Get the number of pedestrians from a ROS parameter, defaulting to 4 if not set. To control this value, edit state_estimator.launch file
+        num_pedestrians = rospy.get_param("~num_pedestrians", 4)
+        
+        # Initialize the StateEstimator with the specified number of pedestrians
+        state_updater = StateUpdater(num_pedestrians)
+
         print("\n--- hello from state_updater.py ---\n")
 
-        StateUpdater()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
